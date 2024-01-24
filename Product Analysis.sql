ee84a33728850789f8cdc248d43bf88150910208
annieclee <156398144+annieclee@users.noreply.github.com>
2024-01-23 21:34:19 -0600

    Update Product Analysis.sql

--- Product Analysis.sql
+++ Product Analysis.sql
@@ -3,48 +3,49 @@
 
 --Analyzing top brands based on total sum of loves_count (user 'favorite' feature on website)
 --Also displaying average product rating (rounded) + total # of reviews for each brand
-select
+SELECT
 	brand_name as 'Brand',
 	sum(loves_count) as 'Total Loves',
 	round(avg(rating),2) as 'Average Product Rating',
 	sum(reviews) as 'Total Reviews'
-from product_info
-group by brand_name
-order by 2 desc;
+FROM product_info
+GROUP BY brand_name
+ORDER BY 2 DESC;
 --1. Sephora Collection, 2. Fenty Beauty by Rihanna, 3. Anastasia Beverly Hills, 4. Charlotte Tilbury, 5. The Ordinary
 
 --Identifying products with highest and lowest ratings (over 50 reviews)
-select
+SELECT
 	product_name,
 	brand_name,
 	round(rating,2) as 'rating',
 	reviews
-from product_info
-where rating = (
-	select max(rating) from product_info
-	where reviews > 50
+FROM product_info
+WHERE rating = (
+	SELECT max(rating)
+	FROM product_info
+	WHERE reviews > 50
 	)
 --Highest rating: BondiBoost Rapid Repair Hair Mask
 
-select
+SELECT
 	product_name,
 	brand_name,
 	round(rating,2) as 'rating',
 	reviews
-from product_info
-where rating = (
-	select min(rating) from product_info
-	where reviews > 50
+FROM product_info
+WHERE rating = (
+	SELECT min(rating) FROM product_info
+	WHERE reviews > 50
 	)
 --Lowest rating: Sephora LashCraft Mascara
 
 --Calculating the average, maximum, and minimum prices for each product category
-select
+SELECT
 	tertiary_category as 'Product Type',
 	avg(price_usd) as 'Average Price',
 	min(price_usd) as 'Minimum Price',
 	max(price_usd) as 'Maximum Price'
-from product_info
-where tertiary_category is not null
-group by tertiary_category
-order by 1;
+FROM product_info
+WHERE tertiary_category IS NOT NULL
+GROUP BY tertiary_category
+ORDER BY 1;
